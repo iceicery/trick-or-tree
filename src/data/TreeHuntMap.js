e24a4880368ca1268627d3aeec7606604f3fd427
pinwei wu <pw.amy.wu@gmail.com>
2021-04-19 09:28:15 -0500

    add treepins data for treehuntmap

---
+++ src/data/TreeHuntMap.js
@@ -0,0 +1,10 @@
+export const treepins = [
+  { name: 'Floating clouds maple', coor: [34.707157, -86.632107] },
+  { name: 'Spanish Oak', coor: [34.708008, -86.634358] },
+  { name: 'Winged elm', coor: [34.706788, -86.634162] },
+  { name: 'September elm', coor: [34.707107, -86.634231] },
+  { name: 'Red cedar', coor: [34.707261, -86.634346] },
+  { name: 'Scarlet oak', coor: [34.707666, -86.634285] },
+  { name: 'Southern red oak', coor: [34.707714, -86.634333] },
+  { name: 'Old field pine', coor: [34.707951, -86.634298] },
+];
