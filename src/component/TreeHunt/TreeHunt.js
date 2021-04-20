3f57b41d0206e87a1f79cb9abf693bc3422db667
pinwei wu <pw.amy.wu@gmail.com>
2021-04-20 15:41:19 -0500

    add right-left button to selection

--- src/component/TreeHunt/TreeHunt.js
+++ src/component/TreeHunt/TreeHunt.js
@@ -1,10 +1,37 @@
 import './TreeHunt.css';
 import lightbulb from '../../images/lightbulb-solid.svg';
+import left from '../../images/left.svg';
+import right from '../../images/right.svg';
 import Tips from '../Tips/Tips';
 import Selection from '../Selection/Selection';
-import { leafType, leafEdge, leafArrange } from '../../data/LeafClass';
+import {
+  leafType,
+  leafEdge,
+  leafArrange,
+  leafShape,
+} from '../../data/LeafClass';
+import { useState } from 'react';
 
 export default function TreeHunt({ toggleTips, isTipsOpen }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalSelectPage = 2;
+
+  function moveRight() {
+    if (currentIndex >= totalSelectPage - 1) {
+      setCurrentIndex(totalSelectPage - 1);
+      return;
+    }
+    setCurrentIndex(currentIndex + 1);
+  }
+
+  function moveLeft() {
+    if (currentIndex <= 0) {
+      setCurrentIndex(0);
+      return;
+    }
+    setCurrentIndex(currentIndex - 1);
+  }
+
   return (
     <section className="treehunt">
       <Tips toggleTips={toggleTips} isTipsOpen={isTipsOpen} />
@@ -14,13 +41,42 @@ export default function TreeHunt({ toggleTips, isTipsOpen }) {
           <img src={lightbulb} alt="tips" className="coddenter__button-img" />
         </button>
       </div>
-      <Selection title={leafType.title} choices={leafType.choices} n={4} />
-      <Selection title={leafEdge.title} choices={leafEdge.choices} n={6} />
-      <Selection
-        title={leafArrange.title}
-        choices={leafArrange.choices}
-        n={2}
-      />
+      <div
+        className="treehunt__container"
+        style={{
+          transform: `translateX(calc(100vw * -${currentIndex}))`,
+        }}
+      >
+        <div className="treehunt__items">
+          <Selection title={leafType.title} choices={leafType.choices} n={4} />
+          <Selection title={leafEdge.title} choices={leafEdge.choices} n={6} />
+          <Selection
+            title={leafArrange.title}
+            choices={leafArrange.choices}
+            n={2}
+          />
+          <button className="treehunt__button" onClick={moveRight}>
+            Next
+            <img
+              src={right}
+              alt="next right"
+              className="treehunt__button-img"
+            />
+          </button>
+        </div>
+        <div className="treehunt__items">
+          <Selection
+            title={leafShape.title}
+            choices={leafShape.choices}
+            n={4}
+          />
+          <button className="treehunt__button" onClick={moveLeft}>
+            <img src={left} alt="next left" className="treehunt__button-img" />
+            Back
+          </button>
+          <button className="treehunt__button-check">Check</button>
+        </div>
+      </div>
     </section>
   );
 }
